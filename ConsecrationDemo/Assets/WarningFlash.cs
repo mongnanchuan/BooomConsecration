e1ccf4be6a1bd4574d6061e51ff2e54d26780bef
mongnanchuan <1349890387@qq.com>
2025-05-03 17:31:51 +0800

    近战与掉落预警闪烁

---
+++ ConsecrationDemo/Assets/WarningFlash.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class WarningFlash : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DOVirtual.Color(new Color(1f, 1f, 1f, 0.8f), new Color(1f, 0f, 0f, 0.8f), 0.4f, c =>
+        {
+            GetComponent<SpriteRenderer>().material.color = c;
+        }).SetLoops(-1, LoopType.Yoyo);
+        /*        Sequence s = DOTween.Sequence();
+                s.SetDelay(0.2f);
+                s.Append(DOVirtual.Color(new Color(1f, 1f, 1f, 0.75f), new Color(1f, 0f, 0f, 0.75f), 0.8f, c => {GetComponent<SpriteRenderer>().material.color = c;}));
+                s.AppendInterval(0.2f);
+                s.SetLoops(-1, LoopType.Yoyo);*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
